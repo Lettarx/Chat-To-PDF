2a48bdd2912fecef208118c2bb486886215ae84f
Alejandra Codocedo <78942170+Lettarx@users.noreply.github.com>
2024-03-04 10:08:11 -0300

    Componentes svelte

---
+++ src/components/App.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { Alert, P } from 'flowbite-svelte';
+    import { APP_STATUS, appStatus } from '../store'
+    import StepLoading from './StepLoading.svelte';
+    import StepChat from './StepChat.svelte';
+    import StepUpload from './StepUpload.svelte';
+</script>
+
+<!-- EL $ en la store es para observar el valor que tienen -->
+
+{#if $appStatus == APP_STATUS.INIT}
+    <StepUpload/>
+{:else if $appStatus == APP_STATUS.LOADING}
+    <StepLoading/>
+{:else if $appStatus == APP_STATUS.CHAT_MODE}
+    <StepChat/>
+{:else if $appStatus == APP_STATUS.ERROR}
+    <Alert>
+        <span class="font-medium">Algo malo a pasado!</span>
+        Hay un error en la aplicación
+    </Alert>
+{:else}
+    <Alert>
+        <span class="font-medium">Acción no reconocida</span>
+        Este estado de la aplicación no está conocido
+    </Alert>
+{/if}
+
+
+
---
+++ src/components/StepUpload.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { appStatus, setAppStatusChatMode, setAppStatusError, setAppStatusLoading } from '../store'
+    import Dropzone from "svelte-file-dropzone";
+  
+    let files = {
+      accepted: [],
+      rejected: []
+    };
+  
+    async function handleFilesSelect(e) {
+      const { acceptedFiles, fileRejections } = e.detail;
+      console.log(acceptedFiles)
+      files.accepted = [...files.accepted, ...acceptedFiles];
+      files.rejected = [...files.rejected, ...fileRejections];
+
+      if (acceptedFiles.length > 0){
+        const formData = new FormData()
+        formData.append('file',acceptedFiles[0])
+        setAppStatusLoading()
+        const res = await fetch('/api/upload',{
+            method: 'POST',
+            body: formData
+        })
+        if (!res.ok){
+             setAppStatusError()
+             return
+        }
+        const { id, url, pages } = await res.json()
+        setAppStatusChatMode({ id, url, pages})
+      }
+    }
+  </script>
+  {#if files.accepted.length == 0}
+    <Dropzone 
+        accept="application/pdf" 
+        multiple={false}
+        on:drop={handleFilesSelect}>Arrastra y suelta aqui tu PDF
+    </Dropzone>
+  {/if}
+
+  <ol>
+    {#each files.accepted as item}
+      <li>{item.name}</li>
+    {/each}
+  </ol>
